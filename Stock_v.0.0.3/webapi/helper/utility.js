d16366d9eb5c7a9934d78208eafd113610a77b3c
AymarBly5591 <100754609+AymarBly5591@users.noreply.github.com>
2022-06-05 23:25:57 +0000

    Delete Stock_v.0.0.3 directory

--- Stock_v.0.0.3/webapi/helper/utility.js
+++
@@ -1,8 +0,0 @@
-const crypto =  require('crypto')
-
-
-function randomTokenString(){
-    return crypto.randomBytes(4).toString('hex').toLocaleUpperCase();
-}
-module.exports =  {randomTokenString};
-
--- Stock_v.0.0.3/webapp/src/routes/DashBoard.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import UserList from '../components/UserList'
-
-export default function DashBoard() {
-  return (
-      <UserList to='/addUser' toEdit='/edit' toStop='/stopUser' toDel='/delete' toInfo='/userInfo'/> 
-  )
-}
